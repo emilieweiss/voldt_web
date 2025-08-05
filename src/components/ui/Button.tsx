a4dd0a212efcb0892e0e07aa8fd2d00d2aad3e4e
katllv <katharina.vu@twoday.com>
2025-08-05 13:05:18 +0200

    destructiveround

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
-  variant?: 'default' | 'secondary' | 'destructive' | 'round';
+  variant?: 'default' | 'secondary' | 'destructive' | 'round' | 'destructiveRound';
 };
 
 function Button({
@@ -17,7 +17,8 @@ function Button({
       'bg-(--color-wolt-blue) hover:bg-(--color-wolt-medium-blue) text-white rounded-xl',
     secondary: 'bg-gray-200 hover:bg-gray-300 text-gray-800 rounded-xl',
     destructive: 'bg-red-700 hover:bg-red-600 text-white rounded-xl',
-    round: 'bg-(--color-wolt-blue) hover:bg-(--color-wolt-medium-blue) text-white rounded-full'
+    round: 'bg-(--color-wolt-blue) hover:bg-(--color-wolt-medium-blue) text-white rounded-full',
+    destructiveRound: 'bg-red-700/40 hover:bg-red-700/80 text-white rounded-full'
   };
 
   return (
